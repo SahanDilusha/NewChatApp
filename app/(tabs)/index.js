fae86977b2bddcb3ef2230fcd10ffd84cc24d34e
SahanDilusha <sahancodelygroup@gmail.com>
2024-12-01 22:47:20 +0530

    "Added Pressable components, AntDesign icon, and useState hook to Index.js; modified styles and layout"

--- app/(tabs)/index.js
+++ app/(tabs)/index.js
@@ -1,9 +1,13 @@
-import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from "react-native";
+import { Keyboard, KeyboardAvoidingView, Platform, Pressable, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Image } from 'expo-image';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { useState } from "react";
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function Index() {
+
+    const [show, setShow] = useState(true);
 
     const googleIcon = require("../../assets/images/Google_Pay-Logo.png");
     const facebookIcon = require("../../assets/images/Facebook-Logo.png");
@@ -16,6 +20,9 @@ export default function Index() {
             <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined} style={styles.container}>
                 <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
                     <View style={[styles.container, styles.main, styles.alignItemsCenter]}>
+                        <Pressable style={[styles.justifyContentCenter,styles.alignItemsCenter,styles.pressable2]}>
+                            <AntDesign name="arrowleft" size={24} color="black" />
+                        </Pressable>
                         <View style={[styles.titleView, styles.justifyContentCenter, styles.alignItemsCenter]}>
                             <Text style={[styles.title, styles.carosBold, styles.h1]}>Log in to Chatbox</Text>
                             <Text style={[styles.textAlignCenter, styles.subTitle, styles.carosLight]}>Welcome back! Sign in using your social account or email to continue us</Text>
@@ -32,10 +39,22 @@ export default function Index() {
                                 <TextInput style={[styles.input, styles.carosMedium, styles.subTitle]} inputMode="email" />
                             </View>
                             <View style={[styles.w_100]}>
-                                <Text style={[styles.carosMedium, styles.subTitle, styles.color]}>Email</Text>
-                                <TextInput style={[styles.input, styles.carosMedium, styles.subTitle]} inputMode="email" />
-                                <FontAwesome name="eye" size={24} color="black" />
+                                <Text style={[styles.carosMedium, styles.subTitle, styles.color]}>Password</Text>
+                                <TextInput style={[styles.input, styles.carosMedium, styles.subTitle]} secureTextEntry={show} />
+                                <Pressable style={styles.eyeIcon} onPress={() => {
+                                    setShow(!show);
+                                }}>
+                                    <FontAwesome name={show ? "eye-slash" : "eye"} size={24} color="#CDD1D3" />
+                                </Pressable>
                             </View>
+                        </View>
+                        <View style={[styles.justifyContentCenter, styles.alignItemsCenter, styles.gap10, styles.w_100, styles.pressableView]}>
+                            <Pressable style={[styles.alignItemsCenter, styles.justifyContentCenter, styles.w_100, styles.pressable1]}>
+                                <Text style={[styles.carosMedium, styles.subTitle, styles.color2]}>Login</Text>
+                            </Pressable>
+                            <Pressable style={[styles.justifyContentCenter, styles.alignItemsCenter, styles.w_100]}>
+                                <Text style={[styles.carosMedium, styles.subTitle, styles.color]}>Forgot Password?</Text>
+                            </Pressable>
                         </View>
                     </View>
                 </TouchableWithoutFeedback>
@@ -51,6 +70,7 @@ const styles = StyleSheet.create({
     main: {
         backgroundColor: "#ffffff",
         paddingHorizontal: 20,
+        paddingVertical:20,
     },
     alignItemsCenter: {
         alignItems: "center",
@@ -112,8 +132,28 @@ const styles = StyleSheet.create({
         borderStyle: 'solid',
         borderBottomColor: "#CDD1D0",
         borderBottomWidth: 2,
+        paddingHorizontal: 30,
     },
     gap10: {
         gap: 40,
+    },
+    eyeIcon: {
+        position: "absolute",
+        right: 0,
+        top: 30,
+    },
+    color2: {
+        color: "#fff",
+    },
+    pressable1: {
+        backgroundColor: "#24786D",
+        height: 50,
+        borderRadius: 20,
+    },
+    pressableView: {
+        bottom: -80,
+    },
+    pressable2:{
+        alignSelf:"flex-start",
     },
 });
